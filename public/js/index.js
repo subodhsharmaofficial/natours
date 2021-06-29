c87cde40ea2256cd6619c4abced954d86a217a54
Subodh Sharma <subodhsharmaofficial@gmail.com>
2021-06-29 22:41:19 +0530

    Initial commit

--- public/js/index.js
+++ public/js/index.js
@@ -12,64 +12,57 @@ const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
 const bookBtn = document.getElementById('book-tour');
+
 // DELEGATION
 if (mapBox) {
   const locations = JSON.parse(mapBox.dataset.locations);
-  // console.log(locations);
   displayMap(locations);
 }
 
-if (loginForm) {
+if (loginForm)
   loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password);
   });
-}
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
 
-if (userDataForm) {
+if (userDataForm)
   userDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const form = new FormData();
     form.append('name', document.getElementById('name').value);
-
     form.append('email', document.getElementById('email').value);
-
     form.append('photo', document.getElementById('photo').files[0]);
-
     console.log(form);
 
     updateSettings(form, 'data');
   });
 
-  if (userPasswordForm)
-    userPasswordForm.addEventListener('submit', async (e) => {
-      e.preventDefault();
-      document.querySelector('.btn--save-password').textContent = 'Updating...';
-      const passwordCurrent = document.getElementById('password-current').value;
-      const password = document.getElementById('password').value;
-      const passwordConfirm = document.getElementById('password-confirm').value;
-      const email = document.getElementById('email').value;
-      await updateSettings(
-        { passwordCurrent, password, passwordConfirm },
-        'password'
-      );
+if (userPasswordForm)
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
 
-      document.querySelector('.btn--save-password').textContent =
-        'Save password';
-      document.getElementById('password-current').value = '';
-      document.getElementById('password').value = '';
-      document.getElementById('password-confirm').value = '';
-    });
-}
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
 
-if (bookBtn) {
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
+
+if (bookBtn)
   bookBtn.addEventListener('click', (e) => {
     e.target.textContent = 'Processing...';
     const { tourId } = e.target.dataset;
     bookTour(tourId);
   });
-}
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
+
 const router = express.Router();
 
-router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewsController.getOverview
+);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 // /login
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
@@ -11,6 +18,8 @@ router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/signup', viewsController.getSignUpForm);
 
 router.get('/me', authController.protect, viewsController.getAccount);
+
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
 
 router.post(
   '/submit-user-data',
